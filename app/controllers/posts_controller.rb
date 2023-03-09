0725c541d244c93d9d8c2849d26b39a5beaf8cb4
Maxime Van Eygen <maximevaneygen@gmail.com>
2023-03-09 15:53:51 +0100

    assets change

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
   
   def create
     post = Post.create(post_params)
-    redirect_to post_path(post.id)
+    redirect_to posts_path
   end
 
   private
